711b2c3240b62fd3e56c9a84efad08cc318e00f1
xovzd <144159255+2youb00@users.noreply.github.com>
2023-09-28 17:28:57 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -30,7 +30,6 @@ navlinks.forEach(navlink=>navlink.addEventListener('click',()=>{
 let header=document.querySelector('.header');
 window.addEventListener('scroll',()=>{
     if(this.scrollY>=50){
-        console.log('ss');
         header.classList.add('scroll-header');
     }
     else{
@@ -40,6 +39,9 @@ window.addEventListener('scroll',()=>{
 ////////////////////////////////popular//////////////////////////////////////////////
 let popularcard=document.querySelector('.popular-card');
 ///popularcard.style.width='256px';
+
+//boll.forEach(cell => {cell.classList.add ('sss');});
+
 /*--------------------لي نقلتها تاع تحريك العنصر  Initialize Swiper--------------------*/
 var swiper = new Swiper(".mySwiper", {
         loop:true,
@@ -60,4 +62,21 @@ var swiper = new Swiper(".mySwiper", {
         },
       },
      });
-     
+////////////////////////////////popular end//////////////////////////////////////////////
+////////////////////////////////featured//////////////////////////////////////////////
+//////////////////// مكتبة
+let mixerFeatured = mixitup('.featured-content', {
+        selectors: {
+            target: '.featured-card'
+        },
+        animation: {
+            duration: 300
+        }
+    });
+/////////////////////////
+let activefeatured=document.querySelectorAll('.featured-item');
+function activefeaturedf(){
+activefeatured.forEach(active=>active.classList.remove('active-featured'))
+this.classList.add('active-featured');
+}
+activefeatured.forEach(active=>active.addEventListener('click',activefeaturedf));
